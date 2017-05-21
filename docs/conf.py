e8ff5073c89c56eb6e93d8af3d056d38a869b7fc
Python3pkg <raliclo@gmail.com>
2017-05-21 13:42:37 -0700

    Convert to Python3

--- docs/conf.py
+++ docs/conf.py
@@ -55,8 +55,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'PyCssMiniFy'
-copyright = u'2015, Shantanu Khandelwal'
+project = 'PyCssMiniFy'
+copyright = '2015, Shantanu Khandelwal'
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
@@ -209,8 +209,8 @@ latex_elements = {
 # [howto/manual]).
 latex_documents = [
     ('index', 'PyCssMiniFy.tex',
-     u'PyCssMiniFy Documentation',
-     u'Shantanu Khandelwal', 'manual'),
+     'PyCssMiniFy Documentation',
+     'Shantanu Khandelwal', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at
@@ -240,8 +240,8 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'PyCssMiniFy',
-     u'PyCssMiniFy Documentation',
-     [u'Shantanu Khandelwal'], 1)
+     'PyCssMiniFy Documentation',
+     ['Shantanu Khandelwal'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -255,8 +255,8 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     ('index', 'PyCssMiniFy',
-     u'PyCssMiniFy Documentation',
-     u'Shantanu Khandelwal',
+     'PyCssMiniFy Documentation',
+     'Shantanu Khandelwal',
      'PyCssMiniFy',
      'One line description of project.',
      'Miscellaneous'),
